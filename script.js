74e2ef7e70e6cbc1afbc0b872d3dd57e52c190ed
Captaincrogs <2003829@talnet.nl>
2020-01-31 15:53:17 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+//document.querySelector(".h1tag").innerHTML = "All the elements";{
+var matches = document.querySelectorAll("h1");{
+matches[0].style.color = "blue";
+}
+
+var matches = document.querySelectorAll(".h1tag");
+function myFunction(){    
+    var kleur = document.getElementById("kleur").value;
+return document.body.style.backgroundColor = kleur;
+}
+
+
+
+
